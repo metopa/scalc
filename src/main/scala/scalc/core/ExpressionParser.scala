f9a4b733bc6dfecc34183fab6a4d79a15f496ef1
Viacheslav Kroilov <slavakroilov@gmail.com>
2018-05-24 09:31:07 +0200

    Fix parser

--- src/main/scala/scalc/core/ExpressionParser.scala
+++ src/main/scala/scalc/core/ExpressionParser.scala
@@ -1,64 +1,86 @@
 package scalc.core
 
-import scalc.core.operands.{binop, unop, Constant, Value}
+import scalc.core.operands.{Constant, Value, binop, unop}
 
 import scala.util.parsing.combinator._
 
 object ExpressionParser extends RegexParsers {
   override def skipWhitespace = true
 
-  def parseExpr(expr: String): Value = {
-    parse(E, expr) match {
-      case Success(value, _) => value
-      case Failure(msg, _) => throw new ParsingError(msg)
-      case Error(msg, _) => throw new ParsingError(msg)
+  def reduceOp(operands: List[~[Value, (Value, Value) => Value]]): Value = {
+    operands match {
+      case (lhs ~ op) :: (rhs ~ op2) :: tail =>
+        reduceOp(new ~(op(lhs, rhs), op2) :: tail)
+      case (value ~ _) :: Nil => value
+      case Nil                => throw new AssertionError("Unreachable")
     }
   }
 
-  private def E: Parser[Value] = FPlusE | FMinusE | F
-  private def F: Parser[Value] = GMultiplyF | GDivF | GModF | GPowF | G
-  private def G: Parser[Value] = plusT | minusT | T
+  def parseExpr(expr: String): Value = {
+    parse(E1, expr) match {
+      case Success(value, _) => value
+      case Failure(msg, _)   => throw new ParsingError(msg)
+      case Error(msg, _)     => throw new ParsingError(msg)
+    }
+  }
+
+  private def E1: Parser[Value] = ((E2 ~ binop1).* ~ E2) ^^ {
+    case l ~ f => reduceOp(l :+ new ~(f, (x: Value, y: Value) => x))
+  }
+
+  private def E2: Parser[Value] = ((E3 ~ binop2).* ~ E3) ^^ {
+    case l ~ f => reduceOp(l :+ new ~(f, (x: Value, y: Value) => x))
+  }
+
+  private def E3: Parser[Value] = ((F ~ binop3).* ~ F) ^^ {
+    case l ~ f => reduceOp(l :+ new ~(f, (x: Value, y: Value) => x))
+  }
+
+  private def F: Parser[Value] = (opt(unop1) ~ T) ^^ {
+    case Some(op) ~ t => op(t)
+    case None ~ t     => t
+  }
   private def T: Parser[Value] = number | parenthesizedE | absE
 
-  private def FPlusE: Parser[Value] = (F ~ "+" ~ E) ^^ {
-    case f ~ "+" ~ e => new binop.Plus(f, e)
+  private def binop1: Parser[(Value, Value) => Value] = ("+" | "-") ^^ {
+    case "+" =>
+      (a, b) =>
+        new binop.Plus(a, b)
+    case "-" =>
+      (a, b) =>
+        new binop.Minus(a, b)
   }
 
-  private def FMinusE: Parser[Value] = (F ~ "-" ~ E) ^^ {
-    case f ~ "-" ~ e => new binop.Minus(f, e)
+  private def binop2: Parser[(Value, Value) => Value] =
+    ("*" | "/" | "%") ^^ {
+      case "*" =>
+        (a, b) =>
+          new binop.Multiply(a, b)
+      case "/" =>
+        (a, b) =>
+          new binop.Div(a, b)
+      case "%" =>
+        (a, b) =>
+          new binop.Mod(a, b)
+    }
+
+  private def binop3: Parser[(Value, Value) => Value] =
+    "^" ^^ { _ => (a, b) =>
+      new binop.Pow(a, b)
+    }
+
+  private def unop1: Parser[Value => Value] = ("+" | "-") ^^ {
+    case "+" =>
+      a =>
+        new unop.Plus(a)
+    case "-" =>
+      a =>
+        new unop.Minus(a)
   }
 
-  private def GMultiplyF: Parser[Value] = (G ~ "*" ~ F) ^^ {
-    case g ~ "*" ~ f => new binop.Multiply(g, f)
-  }
+  private def parenthesizedE: Parser[Value] = "(" ~> E1 <~ ")"
 
-  private def GDivF: Parser[Value] = (G ~ "/" ~ F) ^^ {
-    case g ~ "/" ~ f => new binop.Div(g, f)
-  }
-
-  private def GModF: Parser[Value] = (G ~ "%" ~ F) ^^ {
-    case g ~ "%" ~ f => new binop.Mod(g, f)
-  }
-
-  private def GPowF: Parser[Value] = (G ~ "^" ~ F) ^^ {
-    case g ~ "^" ~ f => new binop.Pow(g, f)
-  }
-
-  private def plusT: Parser[Value] = ("+" ~ T) ^^ {
-    case "+" ~ t => new unop.Plus(t)
-  }
-
-  private def minusT: Parser[Value] = ("-" ~ T) ^^ {
-    case "-" ~ t => new unop.Minus(t)
-  }
-
-  private def parenthesizedE: Parser[Value] = ("(" ~ E ~ ")") ^^ {
-    case "(" ~ e ~ ")" => e
-  }
-
-  private def absE: Parser[Value] = ("|" ~ E ~ "|") ^^ {
-    case "|" ~ e ~ "|" => new unop.Abs(e)
-  }
+  private def absE: Parser[Value] = ("|" ~> E1 <~ "|") ^^ (e => new unop.Abs(e))
 
   private def number: Parser[Value] =
     """\d+(\.\d*)?""".r ^^ { s: String =>
--- src/test/scala/scalc/core/ExpressionParserTest.scala
+++ src/test/scala/scalc/core/ExpressionParserTest.scala
@@ -14,5 +14,11 @@ class ExpressionParserTest extends FunSuite {
     assert(ExpressionParser.parseExpr("2 + 9 / 3 - 3").evaluate() === 2)
     assert(ExpressionParser.parseExpr("2 + 9 % 4 - 3").evaluate() === 0)
     assert(ExpressionParser.parseExpr("2 + 3 ^ 2 - 3").evaluate() === 8)
+    assert(ExpressionParser.parseExpr("2 + 3 ^ |2 - 3|").evaluate() === 5)
+    assert(ExpressionParser.parseExpr("2 * (3 + 2 ^ 2) ^ 2").evaluate() === 98)
+    assert(
+      ExpressionParser
+        .parseExpr("(1 + 2) * |3 * 5 + 7| - 3 * 2 ^ 4")
+        .evaluate() === 18)
   }
 }
