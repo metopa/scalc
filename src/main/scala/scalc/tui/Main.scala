11fac5157dc3c7c2c40b985bd22869b61f06d9a4
Viacheslav Kroilov <slavakroilov@gmail.com>
2018-05-24 10:33:47 +0200

    Add simple repl program

---
+++ src/main/scala/scalc/tui/Main.scala
@@ -0,0 +1,28 @@
+package scalc.tui
+
+import scalc.core.SCalc
+
+object Main extends App {
+  val calc = new SCalc()
+
+
+  def repl(): Boolean = {
+    val input = scala.io.StdIn.readLine(":>")
+    input match {
+      case null => false
+      case "exit" => false
+      case _ =>
+        try {
+          val ans = calc.evaluate(input)
+          println(ans)
+          true
+        } catch {
+          case e: Exception => println(e); true
+        }
+    }
+
+  }
+
+  println("SCalc")
+  while (repl()) {}
+}
